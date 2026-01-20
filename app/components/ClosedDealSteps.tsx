677161936361cb31b30b5fd8617e0405485ce6cb
dhavalzindex <dhaval.zindexsolutions@gmail.com>
2026-01-20 10:33:12 +0530

    nancy page progressbar section simple design done

--- app/components/ClosedDealSteps.tsx
+++ app/components/ClosedDealSteps.tsx
@@ -14,56 +14,56 @@ const ClosedDealSteps = () => {
 
                         <div className="block">
                             <div className="flex flex-wrap">
-                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-26 before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600">
-                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600">
-                                        <div className="block text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-gray300 pb-3 lg:pb-10 pl-10 lg:px-4">
+                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-[103px] xl:before:top-[118px] before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600 after:absolute after:top-0 lg:after:top-[103px] xl:after:top-[118px] after:left-4 lg:after:left-0 after:right-0 after:w-[5px] lg:after:w-full after:h-full lg:after:h-[5px] after:bg-purple">
+                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] xl:before:top-[105px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border-3 before:border-white before:bg-purple z-9">
+                                        <div className="block font-medium text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-black100 pb-3 lg:pb-10 pl-10 lg:px-4">
                                             <span>01</span>
                                         </div>
-                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[37px] md:before:-top-11 lg:before:-top-[5px] before:left-3.5 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2.5 before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
+                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[36px] md:before:-top-11 lg:before:-top-[5px] before:left-[15px] lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2 lg:before:w-2.5 before:h-2 lg:before:h-2.5 before:rounded-full before:bg-white pl-10 lg:px-4">
                                             <p>Lead comes in. Any portal, any channel, any time.</p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-26 before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600">
-                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600">
-                                        <div className="block text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-gray300 pb-3 lg:pb-10 pl-10 lg:px-4">
+                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-[103px] xl:before:top-[118px] before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600 after:absolute after:top-0 lg:after:top-[103px] xl:after:top-[118px] after:left-4 lg:after:left-0 after:right-0 after:w-[5px] lg:after:w-full after:h-full lg:after:h-[5px] after:bg-purple">
+                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] xl:before:top-[105px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border-3 before:border-white before:bg-purple z-9">
+                                        <div className="block font-medium text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-black100 pb-3 lg:pb-10 pl-10 lg:px-4">
                                             <span>02</span>
                                         </div>
-                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[37px] md:before:-top-11 lg:before:-top-[5px] before:left-3.5 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2.5 before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
+                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[36px] md:before:-top-11 lg:before:-top-[5px] before:left-[15px] lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2 lg:before:w-2.5 before:h-2 lg:before:h-2.5 before:rounded-full before:bg-white after:absolute after:-bottom-4.5 lg:after:bottom-auto lg:after:-top-[5px] after:left-3.5 lg:after:left-auto lg:after:right-0 after:w-2 lg:after:w-2.5 after:h-2 lg:after:h-2.5 after:rounded-full after:bg-purple pl-10 lg:px-4">
                                             <p>Nancy responds in seconds. Qualifies. Captures preferences.</p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-26 before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600">
-                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600">
+                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-[103px] xl:before:top-[118px] before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600 after:absolute after:top-0 lg:after:top-[103px] xl:after:top-[118px] after:left-4 lg:after:left-0 after:right-0 after:w-[5px] lg:after:w-full after:h-full lg:after:h-[5px] after:bg-transparent">
+                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] xl:before:top-[105px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600 z-9">
                                         <div className="block text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-gray300 pb-3 lg:pb-10 pl-10 lg:px-4">
                                             <span>03</span>
                                         </div>
-                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[37px] md:before:-top-11 lg:before:-top-[5px] before:left-3.5 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2.5 before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
+                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[36px] md:before:-top-11 lg:before:-top-[5px] before:left-[15px] lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2 lg:before:w-2.5 before:h-2 lg:before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
                                             <p>Data syncs to your CRM. Prospect profile builds automatically.</p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-26 before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600">
-                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600">
+                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-[103px] xl:before:top-[118px] before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600 after:absolute after:top-0 lg:after:top-[103px] xl:after:top-[118px] after:left-4 lg:after:left-0 after:right-0 after:w-[5px] lg:after:w-full after:h-full lg:after:h-[5px] after:bg-transparent">
+                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] xl:before:top-[105px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600 z-9">
                                         <div className="block text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-gray300 pb-3 lg:pb-10 pl-10 lg:px-4">
                                             <span>04</span>
                                         </div>
-                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[37px] md:before:-top-11 lg:before:-top-[5px] before:left-3.5 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2.5 before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
+                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[36px] md:before:-top-11 lg:before:-top-[5px] before:left-[15px] lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2 lg:before:w-2.5 before:h-2 lg:before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
                                             <p>Nancy matches them to properties across your portfolio. Sends proactive outreach.</p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-26 before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600">
-                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600">
+                                <div className="flex-none w-full lg:w-1/5 py-4 first:pt-0 last:pb-0 lg:py-0 relative before:absolute before:top-0 lg:before:top-[103px] xl:before:top-[118px] before:left-4 lg:before:left-0 before:right-0 before:w-[5px] lg:before:w-full before:h-full lg:before:h-[5px] before:bg-titan-white600 after:absolute after:top-0 lg:after:top-[103px] xl:after:top-[118px] after:left-4 lg:after:left-0 after:right-0 after:w-[5px] lg:after:w-full after:h-full lg:after:h-[5px] after:bg-transparent">
+                                    <div className="block lg:text-center max-w-72 md:max-w-full relative before:absolute before:top-1.5 md:before:bottom-7 lg:before:bottom-auto lg:before:top-[91px] xl:before:top-[105px] before:left-1 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-7.5 before:h-7.5 before:rounded-full before:border before:border-titan-white600 z-9">
                                         <div className="block text-3xl md:text-4xl lg:text-5xl xl:text-58 leading-snug text-gray300 pb-3 lg:pb-10 pl-10 lg:px-4">
                                             <span>05</span>
                                         </div>
-                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[37px] md:before:-top-11 lg:before:-top-[5px] before:left-3.5 lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2.5 before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
+                                        <div className="block small-paragraph leading-snug! pt-3 lg:pt-10 relative before:absolute before:-top-[36px] md:before:-top-11 lg:before:-top-[5px] before:left-[15px] lg:before:left-1/2 lg:before:-translate-x-1/2 before:w-2 lg:before:w-2.5 before:h-2 lg:before:h-2.5 before:rounded-full before:bg-titan-white600 pl-10 lg:px-4">
                                             <p>Viewing booked. Opportunity flagged. Revenue captured.</p>
                                         </div>
                                     </div>
